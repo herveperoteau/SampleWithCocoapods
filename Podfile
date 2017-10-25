4ed47b919dae6d7980439291270d3edcd07fc2c1
Hervé Péroteau <herve.peroteau@gmail.com>
2017-10-25 21:21:21 +0200

    Pod setup

---
+++ Podfile
@@ -0,0 +1,5 @@
+inhibit_all_warnings!
+use_frameworks!
+target 'SampleWithCocoapods' do
+   pod 'TestPodCarthage', :git => 'https://github.com/herveperoteau/TestPodCarthage.git'
+end
--- SampleWithCocoapods/ViewController.swift
+++ SampleWithCocoapods/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import TestPodCarthage
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    let myKit = MyKit()
+    myKit.toto()
   }
 
   override func didReceiveMemoryWarning() {
